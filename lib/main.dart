b92202d929ec48827874e9982e983e4df7052089
Ram54218 <rameshram2294@gmail.com>
2025-05-09 12:24:26 +0530

    added data in first

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "datt",
+      title: "App Data",
       theme: ThemeData(
         // This is the theme of your application.
         //
